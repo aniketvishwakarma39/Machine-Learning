6c6123a573d7d579d5e10bf8d43f96756b2f573b
unknown <aniketvishwa39@gmail.com>
2026-02-17 01:40:39 +0530

    .

--- supervised learning/Linear Regression/1.py
+++ supervised learning/Linear Regression/1.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sk
+from sklearn.metrics import r2_score, mean_squared_error, confusion_matrix, f1_score, recall_score
 
 
 data = pd.DataFrame({
@@ -11,11 +11,19 @@ data = pd.DataFrame({
 
 print(data)
 
-x=data["Experience"]
-y=data["salary"]
+x=data[["Experience"]]
+y=data["Salary"]
 
 x_train, x_test, y_train, y_test= train_test_split(x,y, test_size=0.25, random_state=42)
 
 model=LinearRegression()
 
 model.fit(x_train, y_train)
+y_pred= model.predict(x_test)
+
+accuracy= r2_score(y_test, y_pred)
+print(accuracy*100)
+
+mse= mean_squared_error(y_test, y_pred)
+
+print(mse)
